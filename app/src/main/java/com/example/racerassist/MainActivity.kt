a83aba8948a4520309ea1f864f83a858136ee594
DozyC <dozyc@users.noreply.github.com>
2019-11-06 23:43:23 -0600

    Get “HTML” that’s actually still javascript, show in AlertDialog. Copy pasta from https://stackoverflow.com/a/8201246

--- app/src/main/java/com/example/racerassist/MainActivity.kt
+++ app/src/main/java/com/example/racerassist/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.example.racerassist
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.StrictMode
 import android.util.Log
+import android.webkit.JavascriptInterface
 import com.microsoft.signalr.HubConnection
 import com.microsoft.signalr.HubConnectionBuilder
 import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AlertDialog
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 const val race_url = "http://cotaaustin.clubspeedtiming.com/sp_center/livescore.aspx"
 const val race_hub = "http://cotaaustin.clubspeedtiming.com/SP_Center/signalr"
@@ -14,6 +19,21 @@ const val race_hub = "http://cotaaustin.clubspeedtiming.com/SP_Center/signalr"
 class MainActivity : AppCompatActivity() {
 
     lateinit var hubConnection: HubConnection
+
+
+    /* zomg Android Studio auto-converted this from Java code copied from
+     * https://stackoverflow.com/a/8201246
+     */
+    internal inner class MyJavaScriptInterface(private val ctx: Context) {
+
+        @JavascriptInterface
+        fun showHTML(html: String) {
+            AlertDialog.Builder(ctx).setTitle("HTML").setMessage(html)
+                .setPositiveButton(android.R.string.ok, null)
+                .setCancelable(false).create().show()
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,12 +43,23 @@ class MainActivity : AppCompatActivity() {
         StrictMode.setThreadPolicy(policy)
 
         webView.settings.javaScriptEnabled = true
+        webView.addJavascriptInterface(
+            MyJavaScriptInterface(this), "HtmlViewer")
+
+        webView.webViewClient = (object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                webView.loadUrl("javascript:window.HtmlViewer.showHTML" +
+                        "('<html>'+document.getElementsByTagName('html')[0].innerHTML+'</html>');")
+            }
+        })
+
 
         webView.loadUrl(race_url)
 
         hubConnection = HubConnectionBuilder.create(race_hub).build()
         hubConnection.start()
-        Log.i("cvasco.signalr", hubConnection.connectionState.toString())
-        //Log.i("cvasco.signalr", hubConnection)
+        Log.i("dozyc.signalr", hubConnection.connectionState.toString())
+        Log.i("dozyc.html", "TODO: get html in this log...")
+        //Log.i("dozyc.signalr", hubConnection)
     }
 }
