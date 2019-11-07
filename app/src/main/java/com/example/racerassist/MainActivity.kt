b7383868859e27b8a4f4329833995eca9a6bc77e
DozyC <dozyc@users.noreply.github.com>
2019-11-06 21:27:58 -0600

    Trying to use signalR, but hub connection is disconnected (see logcat)…

--- app/src/main/java/com/example/racerassist/MainActivity.kt
+++ app/src/main/java/com/example/racerassist/MainActivity.kt
@@ -3,21 +3,32 @@ package com.example.racerassist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.StrictMode
+import android.util.Log
+import com.microsoft.signalr.HubConnection
+import com.microsoft.signalr.HubConnectionBuilder
 import kotlinx.android.synthetic.main.activity_main.*
 
-val race_url = "http://cotaaustin.clubspeedtiming.com/sp_center/livescore.aspx"
+const val race_url = "http://cotaaustin.clubspeedtiming.com/sp_center/livescore.aspx"
+const val race_hub = "http://cotaaustin.clubspeedtiming.com/SP_Center/signalr"
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var hubConnection: HubConnection
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
-        //StrictMode.setThreadPolicy(policy)
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
 
         webView.settings.javaScriptEnabled = true
 
         webView.loadUrl(race_url)
+
+        hubConnection = HubConnectionBuilder.create(race_hub).build()
+        hubConnection.start()
+        Log.i("cvasco.signalr", hubConnection.connectionState.toString())
+        //Log.i("cvasco.signalr", hubConnection)
     }
 }
